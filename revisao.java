ead04769cab6a426305908a5db4245917b3360aa
Gean Carlos de Sousa Bandeira <60990296+geanbandeira@users.noreply.github.com>
2025-03-19 19:25:45 -0300

    Update revisao.java

--- revisao.java
+++ revisao.java
@@ -1,1 +1,25 @@
 
+import java.io.*;
+import java.util.*;
+
+class Pessoa{
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+    
+    public void apresentar(){
+        System.out.println("Nome: " + nome + "Idade: " + idade);
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+}
+
+class Funcionario extends Pessoa{
+    
+}
